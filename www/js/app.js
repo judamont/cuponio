c294546ed9a6698e51f3538e53b64633a2c60882
juliano <jaspart@gmail.com>
2015-02-06 16:37:45 -0500

    Inicio cuponio

---
+++ www/js/app.js
@@ -0,0 +1,33 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+console.log("cargo");
+$("#hoy").on("load", function (event) {
+    console.log("navigated!");
+});
+
+$(document).on("pagebeforecreate", function (event) {
+    console.log("antes de crear");
+});
+$(document).on("pagecreate", function (event) {
+    console.log("pagina creada");
+});
+
+$(document).ready(function () {
+    $("#a_hoy").click(function () {
+        alert('cargando cupones');
+        $.mobile.navigate("#hoy");
+    });
+    $("#a_mis_cupones").click(function () {
+        alert('cargando mis cupones');
+        $.mobile.navigate("#mis_cupones");
+    });
+    $("#a_categorias").click(function () {
+        alert('cargando categorias');
+        $.mobile.navigate("#categorias");
+    });
+});
+
+
---
+++ www/js/util.js
@@ -0,0 +1,16 @@
+var servicio = "http://oxes-cotejo.rhcloud.com/webresources/";
+//var servicio = "http://localhost:8081/webresources/";
+function message(e) {
+    var html = $.parseHTML(e.responseText)
+    if (html != null) {
+        for (var i = 0; i < html.length; i++) {
+            if (html[i].innerHTML != null && html[i].innerHTML.indexOf('java.lang') !== -1) {
+                return html[i].innerHTML.split(":")[1];
+            }
+        }
+    }
+}
+
+function mensajeSoporte() {
+    alert('Error no controlado, contactenos para asistirte o intenta de nuevo.');
+}
