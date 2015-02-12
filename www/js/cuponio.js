d56b77a45450468c626cb2e94f21e9c31c4ec0d0
juliano <jaspart@gmail.com>
2015-02-12 09:21:31 -0500

    Servicio Cupones

--- www/js/cuponio.js
+++ www/js/cuponio.js
@@ -1,6 +1,5 @@
-var servicio = "http://cuponrs-oxes.rhcloud.com/webresources/cupon/get/cuponlist";
-//var servicio = "http://localhost:8081/webresources/";
-
+document.write("<script type='text/javascript' src='util.js'></script>");
+var ultimaImagenPrueba;
 $('#cuponia').bind('pageinit', function (event) {
     getCuponesList();
 });
@@ -8,7 +7,7 @@ $('#cuponia').bind('pageinit', function (event) {
 function getCuponesList() {
 
     $.ajax({
-        url: servicio,
+        url: servicio + 'cupon/get/cupones',
         type: 'GET',
         dataType: 'json',
         contentType: 'application/json',
@@ -16,12 +15,25 @@ function getCuponesList() {
             for (var n = 0; n < resp.length; n++)
             {
                 var object = JSON.parse(resp[n]);
-                $('#cupones').append('<li><a href="#popupBasic" onclick="mostrarCupon(\''+object.nombre+'\');" data-rel="popup" class="ui-btn ui-corner-all ui-shadow ui-btn-inline" data-transition="pop">'+object.nombre+'</a></li>');
+                var linea = '<li><a href="#popupBasic" onclick="mostrarCupon(' + object.codigo + ',\'' + object.nombre + '\');" data-rel="popup" class="ui-btn ui-corner-all ui-shadow ui-btn-inline" data-transition="pop">' + object.nombre;
+                linea += ' <br/><img src="data:image/jpg;base64,';
+                linea += object.imagenBase64 + '"';
+                linea += ' alt="0"';
+                linea += ' style="width:100%;"/>';
+                linea += '</a></li>';
+//                console.log(linea);
+                ultimaImagenPrueba = object.imagenBase64;
+                $('#cupones').append(linea);
             }
         }
     });
 }
 
-function mostrarCupon(nombre){
-    $('#contenidoCupon').html('<p>En este popup va la información del cupon <b>'+nombre+'</b><p>');
+function mostrarCupon(codigo, nombre) {
+    var contenido = '<p>En este popup va la información del cupon  <b>' + codigo +' - '+nombre  + '</b><p>';
+    contenido += ' <br/><img src="data:image/jpg;base64,';
+    contenido += ultimaImagenPrueba + '"';
+    contenido += ' alt="0"';
+    contenido += ' style="width:100%;"/>';
+    $('#contenidoCupon').html(contenido);
 }
