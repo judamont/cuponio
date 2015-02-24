6e1254368af2c1a10cfad35139ffe9e130230bd0
juliano <jaspart@gmail.com>
2015-02-24 11:33:25 -0500

    Consulta Cupon

--- www/js/cuponio.js
+++ www/js/cuponio.js
@@ -1,5 +1,22 @@
 document.write("<script type='text/javascript' src='util.js'></script>");
 var ultimaImagenPrueba;
+
+var cupon = {
+    codigo: 0, nombre: null, descripcion: null, fechaInicio: null, fechaFin: null, imagenBase64: null
+};
+
+var tienda = {
+    codigo: 0, nombre: null, email: null, clave: null
+};
+
+var centroComercial = {
+    codigo: 0, nombre: null, direccion: null
+};
+
+var categoria = {
+    codigo: 0, nombre: null
+};
+
 $('#cuponia').bind('pageinit', function (event) {
     getCuponesList();
 });
@@ -31,9 +48,43 @@ function getCuponesList() {
     });
 }
 
-function mostrarCupon(codCentroComercial,codTienda, codCategoria, codCupon) {
-    
-    var contenido = '<p>En este popup va la información del cupon  <b>' + codCentroComercial +' - '+codCupon  + '</b><p>';
+function mostrarCupon(codCentroComercial, codTienda, codCategoria, codCupon) {
+
+    centroComercial.codigo = codCentroComercial;
+    tienda.codigo = codTienda;
+    cupon.codigo = codCupon;
+    categoria.codigo = codCategoria;
+
+    var data = JSON.stringify({cupon: cupon, centroComercial: centroComercial, tienda: tienda, categoria: categoria});
+
+    $.ajax({
+        url: servicio + 'cupon/get/cupon',
+        type: 'GET',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: data,
+        success: function (resp) {
+//            for (var n = 0; n < resp.length; n++)
+//            {
+//                var object = JSON.parse(resp[n]);
+//                console.log(object.tienda.codigo);
+//                console.log(object.tienda.centroComercial.codigo);
+//                var linea = '<li>\n\<a href="#pre-rendered-page" onclick="mostrarCupon(' + object.codigo + ',\'' + object.nombre + '\');" class="ui-btn ui-corner-all ui-shadow ui-btn-inline"   data-transition="pop">' + object.nombre;
+//                linea += ' <br/><img src="data:image/jpg;base64,';
+//                linea += object.imagenBase64 + '"';
+//                linea += ' alt="0"';
+//                linea += ' style="width:100%;"/>';
+//                linea += '</a></li>';
+////                console.log(linea);
+//                ultimaImagenPrueba = object.imagenBase64;
+//                $('#cupones').append(linea);
+//            }
+//            $.mobile.loading("hide");
+        }
+    });
+
+
+    var contenido = '<p>En este popup va la información del cupon  <b>' + codCentroComercial + ' - ' + codCupon + '</b><p>';
     contenido += ' <br/><img src="data:image/jpg;base64,';
     contenido += ultimaImagenPrueba + '"';
     contenido += ' alt="0"';
@@ -41,6 +92,6 @@ function mostrarCupon(codCentroComercial,codTienda, codCategoria, codCupon) {
     $('#contenidoCupon').html(contenido);
 }
 
-function obtenerCupon(){
-    $('#autorizacionCupon').html('Cup&oacute;n V&aacute;lido');    
+function obtenerCupon() {
+    $('#autorizacionCupon').html('Cup&oacute;n V&aacute;lido');
 }
